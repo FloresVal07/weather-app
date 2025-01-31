5d9510ba22373f107abeb106e5c38c1c419c92ff
FloresVal07 <floresvalentin300@gmail.com>
2025-01-30 18:26:03 -0700

    Added basic properties Temp and Location

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Weather.css"
 import search from "./assets/search.png";
+import clear from "./assets/clear.png";
+import cloud from "./assets/cloud.png";
+import drizzle from "./assets/drizzle.png";
+import rain from "./assets/rain.png";
+import snow from "./assets/snow.png";
+import humidity from "./assets/humidity.png";
 
 
 function Weather(){
@@ -10,6 +16,9 @@ function Weather(){
                 <input type="text" placeholder="search"></input>
                 <img src={search} alt=""></img>
             </div>
+            <img src={clear} alt="" className="weather-icon"></img>
+            <p className="temperature">16°C</p>
+            <p className="location">London</p>
         </div>
     );
 }
